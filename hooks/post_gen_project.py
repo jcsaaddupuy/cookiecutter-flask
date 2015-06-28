58b95ad9c59026f4d4359f02cc08249c9ed73fce
Jean-Christophe SAAD-DUPUY <jc.saaddupuy@fsfe.org>
2015-06-29 00:52:23 +0200

    feat: adds gitignore support

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,14 +8,32 @@ TO_REMOVE={
 'celery': [
         os.path.join(project_directory, project_name, 'tasks.py'),
     ]
+,'git': [
+        os.path.join(project_directory, 'gitignore'),
+    ]
+}
+
+TO_MOVE={
+'git': [
+        (os.path.join(project_directory, 'gitignore'), os.path.join(project_directory, '.gitignore')),
+    ]
 }
 
 def remove_files(files):
     for f in files:
         os.unlink(f)
 
+def move_files(files):
+    for from_, to_ in files:
+        os.rename(from_, to_)
+
 if "{{ cookiecutter.use_celery}}" != 'yes':
     # remove celery files
     remove_files(TO_REMOVE['celery'])
 
+
+if "{{ cookiecutter.use_git}}" != 'yes':
+    remove_files(TO_REMOVE['git'])
+else:
+    move_files(TO_MOVE['git'])
 
