6e8374027c9adc5ee5f14a42fed5cdbfa9ca07db
Jean-Christophe SAAD-DUPUY <jc.saaddupuy@fsfe.org>
2015-06-29 00:06:48 +0200

    fix: fixes appfactory import

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/app.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/app.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 from __future__ import unicode_literals
-import appfactory
+from {{cookiecutter.repo_name}} import appfactory
 
 
 DEFAULT_CONFIG_OBJ='{{cookiecutter.repo_name}}.settings.defaults'
